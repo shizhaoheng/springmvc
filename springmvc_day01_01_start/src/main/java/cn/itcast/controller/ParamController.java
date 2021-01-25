9b29b67f23ef97bf41975d441b5115ed728dfc3a
biubiubiu <2870400383@qq.com>
2021-01-25 21:43:45 +0800

    参数绑定及自定义类型转换

--- springmvc_day01_01_start/src/main/java/cn/itcast/controller/ParamController.java
+++ springmvc_day01_01_start/src/main/java/cn/itcast/controller/ParamController.java
@@ -1,7 +1,8 @@
 package cn.itcast.controller;
 
-//import cn.itcast.domain.Account;
-//import cn.itcast.domain.User;
+import cn.itcast.domain.Account;
+import cn.itcast.domain.User;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -19,33 +20,35 @@ public class ParamController {
 
     /**
      * 请求参数绑定入门
+     *
      * @return
      */
     @RequestMapping("/testParam")
-    public String testParam(String username,String password){
+    public String testParam(String username, String password) {
         System.out.println("执行了...");
-        System.out.println("用户名："+username);
-        System.out.println("密码："+password);
+        System.out.println("用户名：" + username);
+        System.out.println("密码：" + password);
         return "success";
     }
-/*
-    *//**
+
+    /**
      * 请求参数绑定把数据封装到JavaBean的类中
+     *
      * @return
-     *//*
+     */
     @RequestMapping("/saveAccount")
-    public String saveAccount(Account account){
+    public String saveAccount(Account account) {
         System.out.println("执行了...");
         System.out.println(account);
         return "success";
     }
 
 
-    *//**
+    /**
      * 自定义类型转换器
      * @param user
      * @return
-     *//*
+     */
     @RequestMapping("/saveUser")
     public String saveUser(User user){
         System.out.println("执行了...");
@@ -53,7 +56,7 @@ public class ParamController {
         return "success";
     }
 
-    *//**
+    /*/**
      * 原生的API
      * @return
      *//*
